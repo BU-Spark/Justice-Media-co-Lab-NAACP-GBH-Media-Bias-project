76edf131952189039adda8a6487e7dcb0cc9f9c7
temilaj <temilaj@hotmail.com>
2021-02-16 22:20:43 -0500

    setup mongodb database

--- dashboard/back-end/src/index.js
+++ dashboard/back-end/src/index.js
@@ -3,9 +3,13 @@ const dotenv = require("dotenv");
 dotenv.config({});
 
 const app = require("./app");
+const { connectToDB } = require("./config/database");
 const config = require("./config/environments");
 const logger = require("./config/logger");
-const { port, env } = config;
+
+const { port, env, database } = config;
+
+connectToDB(database.URI);
 
 app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
 
