6db10334af35f9b687d25c0957dde953691ce476
Hong Xin <hongxin123456@gmail.com>
2021-03-12 15:42:43 -0500

    Hong_wordcloud_3

--- model/WorldCloud_2/WordCloud_2.py
+++ model/WorldCloud_2/WordCloud_2.py
@@ -11,35 +11,36 @@ from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt 
 import networkx as nx
 from random import randint 
+from itertools import count
+import networkx as nx
+
+# target_word = ['black', 'haitian', 'jamaican', 'dominican', 'women', 'chinese']
+words = pd.read_csv('similar_words.csv')
+# print(words)
+G = nx.Graph()
+
+j = 1
+for i, row in words.iterrows():
+    for j in range(1,len(row)):
+        G.add_node(i,label=row[j])
+    for j in range(1,len(row)):
+        G.add_edge(row[1], row[j])    
+
+# print(G.nodes())
+
+# drawing nodes and edges separately so we can capture collection for colobar
+pos = nx.spring_layout(G,k=0.5)
+betCent = nx.betweenness_centrality(G, normalized=True, endpoints=True)
+node_color = [18000.0 * G.degree(v) for v in G]
+node_size = [v * 10000 for v in betCent.values()]
 
 
-def display_graph():
-    # target_word = ['black', 'haitian', 'jamaican', 'dominican', 'women', 'chinese']
-    words = pd.read_csv('similar_words.csv')
+# G = nx.node_connected_component(G,'black')
+plt.figure(figsize=(15,15))
+ec = nx.draw_networkx_edges(G, pos, alpha=0.8, edge_color = 'black')
+nc = nx.draw_networkx_nodes(G, pos, node_size = node_size, node_color = node_color)
+nx.draw_networkx_labels(G,pos, font_size = 10)
 
-    G = nx.Graph()
-
-    j = 1
-    for i, row in words.iterrows():
-        for j in range(1,len(row)):
-            G.add_node(i,label=row[j])
-        for j in range(1,len(row)):
-            G.add_edge(row[1], row[j])    
-
-
-    plt.rcParams["figure.figsize"] = 10,10
-    options = {
-        "font_size": 12,
-        "node_size": 30,
-        "node_color": "yellow",
-        "edgecolors": "black",
-        "linewidths": 2,
-        "width": 2,
-    }
-    subgraph = nx.node_connected_component(G,'chinese')
-    nx.draw(G.subgraph(subgraph), with_labels=True, **options)
-
-    plt.show()
-
-
-display_graph()
+plt.colorbar(nc)
+plt.axis('off')
+plt.show()
